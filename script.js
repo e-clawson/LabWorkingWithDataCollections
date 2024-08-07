7971d6e2461391b19f503da62a4cc7ef0b102cd0
Elizabeth Clawson <emclawson1@gmail.com>
2024-08-06 19:46:12 -0500

    added final function from Jalita

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// let csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+
 let csv = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
-// const csv = `ID,Name,Occupation,Age 42,Bruce,Knight,41 57,Bob,Fry Cook,19 63,Blaine,Quiz Master,58 98,Bill,Doctor’s Assistant,26`;
-// let cells = csv.split(",");
 
 function feelingLoopy(file) { 
-    rows = file.split("\n")
-    cells = csv.split(",");
+   let rows = file.split("\n")
     numberOfColumns = rows[0].split(',').length;
     for (let i=0; i <rows.length; i++) {
         rows[i] = rows[i].split(",")
@@ -15,7 +12,7 @@ function feelingLoopy(file) {
     console.log(newArray)
 }
 feelingLoopy(csv);
-console.log(newArray)
+console.log(newArray);
 
 // Function to parse and log CSV data
 // let csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
@@ -35,3 +32,48 @@ console.log(newArray)
 // }
 // // Call the function with the CSV data
 // feelingLoopy(csv);
+
+
+let cell = ""; //""
+let row = [];
+let data = [];
+for (let i = 0; i < csv.length; i++) {
+  let currentCharacter = csv[i];
+  //when the currentCharater is comma or string don't add it?
+  if (currentCharacter !== "," && currentCharacter !== "\n") {
+    cell = cell + currentCharacter;
+  }
+  // this is add last cell to the row array then resets so it can hold next cell
+  if ("," === currentCharacter) {
+    row.push(cell);
+    cell = "";
+  }
+  //at the end
+  // this is adds last rows to the data array then resets so it can hold next row
+  else if  (currentCharacter === "\n" || i === csv.length - 1) {
+    row.push(cell);
+    cell = "";
+    data.push(row);
+    row = [];
+  }
+}
+// console.log(data);
+let newData = []
+for (let i =1;i<data.length;i++){
+  let row = data[i]
+console.log(row)
+  let obj = {}
+  obj.id = row[0]
+  obj.name= row[1]
+  obj.occupation = row[2]
+  obj.age = row[3]
+  newData.push(obj)
+}
+  console.log(newData)
+
+
+
+
+
+
+
